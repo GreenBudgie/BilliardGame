45db7fefa57d6f30901a02f1a4cef22472f29886
GreenBudgie <forestengine@yandex.ru>
2024-05-18 19:49:32 +0300

    Add right bar

---
+++ scripts/ui/ScoreCounter.cs
@@ -0,0 +1,16 @@
+﻿public partial class ScoreCounter : CounterLabel
+{
+
+    public override void _Ready()
+    {
+        this.InitAttributes();
+
+        EventBus.Instance.PocketScored += (ball, _) => _HandlePocketScore(ball);
+    }
+
+    private void _HandlePocketScore(PocketBall ball)
+    {
+        Count += ball.Number;
+    }
+
+}
