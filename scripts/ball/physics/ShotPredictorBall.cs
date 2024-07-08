2bcd3c32a729cfa42c7890960995f23aca3dc38c
Dmitry Okishev <dimok4a@mail.ru>
2024-07-08 20:59:36 +0400

    Revert "Added prediction shot length limit"
    This reverts commit 4db3434fa7f68257ea885e79dd435626ee3c08dd.

--- scripts/ball/physics/ShotPredictorBall.cs
+++ scripts/ball/physics/ShotPredictorBall.cs
@@ -3,8 +3,9 @@ using Godot;
 
 public partial class ShotPredictorBall : BallRigidBody
 {
+    
     public const int MaxCollisions = 10;
-
+    
     private readonly List<BallRigidBody> _predictorBallList = new();
 
     public override void _Ready()
@@ -47,8 +48,7 @@ public partial class ShotPredictorBall : BallRigidBody
         return new ShotPrediction(GlobalPosition, _predictedCollisions);
     }
 
-    public ShotPrediction GetShotPredictionWithMaxTrajectoryLength(Vector2 initialVelocity, int maxSteps,
-        Vector2 initialPosition, float maxLength)
+    public ShotPrediction GetShotPredictionWithMaxTrajectoryLength(Vector2 initialVelocity, int maxSteps, Vector2 initialPosition, float maxLength)
     {
         var step = 0;
         var delta = BallPhysicsServer.Instance.DefaultDelta;
@@ -69,7 +69,7 @@ public partial class ShotPredictorBall : BallRigidBody
 
             step++;
         }
-
+        
         return RestrictShotPrediction(initialPosition, maxLength);
     }
 
@@ -81,10 +81,9 @@ public partial class ShotPredictorBall : BallRigidBody
         {
             if ((GlobalPosition - initialPosition).Length() <= maxLength)
                 return new ShotPrediction(GlobalPosition, _predictedCollisions);
-            return new ShotPrediction(initialPosition + ((GlobalPosition - initialPosition).Normalized() * maxLength),
-                _predictedCollisions);
+            return new ShotPrediction(initialPosition + ((GlobalPosition - initialPosition).Normalized() * maxLength), _predictedCollisions);
         }
-
+        
         var totalLength = 0f;
         var lastPoint = initialPosition;
         var newGlobalPosition = new Vector2();
@@ -97,16 +96,14 @@ public partial class ShotPredictorBall : BallRigidBody
                 newGlobalPosition = lastPoint + ((newPoint - lastPoint).Normalized() * (maxLength - totalLength));
                 return new ShotPrediction(newGlobalPosition, _predictedCollisionsWithRestrictedLength);
             }
-
             _predictedCollisionsWithRestrictedLength.Add(collision);
             totalLength += pointsDistance;
             lastPoint = newPoint;
         }
-
         if ((GlobalPosition - _predictedCollisions[^1].ContactPoint).Length() +
             totalLength <= maxLength)
             return new ShotPrediction(GlobalPosition, _predictedCollisionsWithRestrictedLength);
-
+        
         newGlobalPosition = lastPoint + ((GlobalPosition - lastPoint).Normalized() * (maxLength - totalLength));
         return new ShotPrediction(newGlobalPosition, _predictedCollisionsWithRestrictedLength);
     }
