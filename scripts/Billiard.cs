e68712ed166e7af7d45affcbe75cf87d22581a38
GreenBudgie <forestengine@yandex.ru>
2024-08-27 23:48:55 +0300

    Add plus 2 sticker

--- scripts/Billiard.cs
+++ scripts/Billiard.cs
@@ -5,11 +5,12 @@ public partial class Billiard : Node2D
     public override void _Ready()
     {
         var stickerX2 = GD.Load<PackedScene>("res://scenes/sticker/sticker_x2.tscn");
+        var stickerPlus2 = GD.Load<PackedScene>("res://scenes/sticker/sticker_plus_2.tscn");
         foreach (var pocket in PocketManager.Instance.GetPockets())
         {
             var sticker1 = stickerX2.Instantiate<Sticker>();
             pocket.StickerPositions[0].SetSticker(sticker1);
-            var sticker2 = stickerX2.Instantiate<Sticker>();
+            var sticker2 = stickerPlus2.Instantiate<Sticker>();
             pocket.StickerPositions[1].SetSticker(sticker2);
         }
     }
---
+++ scripts/sticker/StickerActionAdd.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+
+public partial class StickerActionAdd : StickerAction
+{
+
+    [Export] private float _points;
+    
+    public override void Trigger(PocketScoreContext context)
+    {
+        context.Score += _points;
+        var effectLabel = EffectLabel.Create();
+        effectLabel.Position += Vector2.Up * 40;
+        effectLabel.Text = $"+{_points}";
+        Sticker.AddChild(effectLabel);
+    }
+
+}
